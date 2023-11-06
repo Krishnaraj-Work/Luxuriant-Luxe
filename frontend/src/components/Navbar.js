1eaafee3d4a42e86e41bf3623716f4566eccb1b1
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-11-07 02:28:44 +0530

    backend added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -15,7 +15,7 @@ import {
 } from "@heroicons/react/24/solid";
 import { NavLink, useNavigate } from "react-router-dom";
 import {
-    IconBrush, IconHanger2,
+    IconBrush, IconGardenCart, IconHanger2,
     IconHome,
     IconKey,
     IconLogout,
@@ -46,9 +46,9 @@ export function Navbar(props) {
     }
 
     return (
-        <div className="pr-4 mr-4 z-50">
+        <div className="z-50">
             <div
-                className={`navbar bg-primary rounded-xl text-primary-content flex-row-reverse justify-between m-4 
+                className={`navbar bg-primary text-primary-content flex-row-reverse justify-between
 			md:flex-row`}
             >
                 {/* name */}
@@ -59,9 +59,9 @@ export function Navbar(props) {
                     <NavLink to={"/"} className="flex flex-row">
                         <div
                             id="luxelogo"
-                            className="w-12 h-12 bg-center snap-center self-center"
+                            className="w-9 h-9 bg-center snap-center self-center"
                         ></div>
-                        <div className="btn btn-ghost normal-case text-lg md:text-2xl">
+                        <div className="md:btn md:btn-ghost md:normal-case md:text-lg md:text-xl hidden md:display">
                             Luxuriant Luxe
                         </div>
                     </NavLink>
@@ -72,9 +72,9 @@ export function Navbar(props) {
 			   md:flex"
                 >
                     <ul className="menu menu-horizontal px-1">
-                        <li className="text-lg md:text-2xl">
+                        <li className="text-lg md:text-xl">
                             <NavLink
-                                to={"/vaults"}
+                                to={"/products"}
                                 id="contact_element"
                                 className="hover:text-black"
                             >
@@ -82,7 +82,7 @@ export function Navbar(props) {
                                 Our Products
                             </NavLink>
                         </li>
-                        <li className="text-lg md:text-2xl">
+                        <li className="text-lg md:text-xl">
                             <NavLink
                                 to={"/about"}
                                 id="contact_element"
@@ -100,7 +100,7 @@ export function Navbar(props) {
                         <ul className="menu menu-horizontal px-1">
                             <li>
                                 <details>
-                                    <summary className="text-lg md:text-2xl hover:text-black">
+                                    <summary className="text-lg md:text-xl hover:text-black">
                                         <IconPalette className="w-8 h-8" />
                                         Theme
                                     </summary>
@@ -141,12 +141,12 @@ export function Navbar(props) {
                                     <ul className="p-2 bg-base-100 text-base-content">
                                         <li className="text-lg">
                                             <NavLink
-                                                to={"/profile"}
+                                                to={"/cart"}
                                                 id="contact_element"
                                                 className="hover:text-black"
                                             >
-                                                {/* <IconLockSquare className="w-8 h-8" /> */}
-                                                Profile
+                                                 <IconGardenCart className="w-8 h-8" />
+                                                Cart
                                             </NavLink>
                                         </li>
                                         <li
@@ -165,58 +165,58 @@ export function Navbar(props) {
                 </div>
 
                 {/* hamburger */}
-                <div className="md:hidden">
-                    <ul className="menu menu-horizontal px-1">
+                <div className="md:hidden w-fit">
+                    <ul className="menu menu-horizontal px-1 w-fit">
                         <li>
-                            <details>
-                                <summary className="text-lg md:text-2xl hover:text-black">
-                                    <Bars3Icon className="w-8 h-8" />
+                            <details className="w-fit">
+                                <summary className="text-md md:text-xl hover:text-black">
+                                    <Bars3Icon className="w-6 h-6" />
                                 </summary>
-                                <ul className="p-2 bg-base-100 text-base-content z-50 flex flex-wrap flex-col w-64 gap-2">
-                                    <li className="text-lg md:text-2xl">
+                                <ul className="p-2 bg-base-100 text-base-content z-50 flex flex-wrap flex-col w-fit gap-2">
+                                    <li className="text-md md:text-xl whitespace-nowrap flex-nowrap w-36">
                                         <NavLink
-                                            to={"/vaults"}
+                                            to={"/products"}
                                             id="contact_element"
                                             className="hover:text-black"
                                         >
-                                            <IconHanger2 className="w-8 h-8" />
-                                            Our Products
+                                            <IconHanger2 className="w-6 h-6" />
+                                            Products
                                         </NavLink>
                                     </li>
-                                    <li className="text-lg md:text-2xl">
+                                    <li className="text-md md:text-xl">
                                         <NavLink
                                             to={"/about"}
                                             id="contact_element"
                                             className="hover:text-black"
                                         >
-                                            <InformationCircleIcon className="w-8 h-8" />
+                                            <InformationCircleIcon className="w-6 h-6" />
                                             About Us
                                         </NavLink>
                                     </li>
                                     <li className="menu menu-horizontal px-1 py-0 ">
                                         <details>
-                                            <summary className="text-lg md:text-2xl hover:text-black">
-                                                <IconBrush className="w-8 h-8" />
+                                            <summary className="text-md md:text-xl hover:text-black">
+                                                <IconBrush className="w-6 h-6" />
                                                 Theme
                                             </summary>
                                             <ul className="p-2 bg-base-100 text-base-content">
                                                 <li
                                                     // data-set-theme="cupcake"
-                                                    className="text-lg"
+                                                    className="text-md"
                                                     onClick={() => setTheme("light")}
                                                 >
                                                     <a>
-                                                        <SunIcon className="w-8 h-8" />
+                                                        <SunIcon className="w-6 h-6" />
                                                         Light
                                                     </a>
                                                 </li>
                                                 <li
-                                                    className="text-lg"
+                                                    className="text-md"
                                                     // data-set-theme="dracula"
                                                     onClick={() => setTheme("dark")}
                                                 >
                                                     <a>
-                                                        <MoonIcon className="w-8 h-6" />
+                                                        <MoonIcon className="w-6 h-6" />
                                                         Dark
                                                     </a>
                                                 </li>
@@ -225,28 +225,28 @@ export function Navbar(props) {
                                     </li>
                                     <li className="menu menu-horizontal px-1 py-0">
                                         <details>
-                                            <summary className="text-lg md:text-2xl hover:text-black">
-                                                <IconUserBolt className="w-8 h-8" />
+                                            <summary className="text-md md:text-xl hover:text-black">
+                                                <IconUserBolt className="w-6 h-6" />
                                                 Account
                                             </summary>
                                             <ul className="p-2 bg-base-100 text-base-content">
-                                                <li className="text-lg">
+                                                <li className="text-md">
                                                     <NavLink
-                                                        to={"/profile"}
+                                                        to={"/cart"}
                                                         id="contact_element"
                                                         className="hover:text-black"
                                                     >
-                                                        <IconUserCircle className="w-8 h-8" />
-                                                        Profile
+                                                        <IconGardenCart className="w-6 h-6" />
+                                                        Cart
                                                     </NavLink>
                                                 </li>
-                                                <li className="text-lg">
+                                                <li className="text-md">
                                                     <NavLink
-                                                        to={"/profile"}
+                                                        to={"/"}
                                                         id="contact_element"
                                                         className="hover:text-black"
                                                     >
-                                                        <IconLogout className="w-8 h-8" />
+                                                        <IconLogout className="w-6 h-6" />
                                                         Logout
                                                     </NavLink>
                                                 </li>
