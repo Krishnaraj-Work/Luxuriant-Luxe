4755468226f061efe8b79b3b0582523140d34b76
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-11-07 02:43:29 +0530

    Added cart logic

---
+++ frontend/src/dev/previews.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {ComponentPreview, Previews} from '@react-buddy/ide-toolbox'
+import {PaletteTree} from './palette'
+import {Navbar} from "../components/Navbar";
+import Home from "../pages/Home";
+
+const ComponentPreviews = () => {
+    return (
+        <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/Navbar">
+                <Navbar/>
+            </ComponentPreview>
+            <ComponentPreview path="/Home">
+                <Home/>
+            </ComponentPreview>
+        </Previews>
+    )
+}
+
+export default ComponentPreviews
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,22 @@ import ReactDOM from "react-dom/client";
 import "./style.css";
 import "./input.css";
 import App from "./App";
-import { BaseUrlProvider } from "./context/BaseUrlContext";
+import {BaseUrlProvider} from "./context/BaseUrlContext";
 
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
+import {DevSupport} from "@react-buddy/ide-toolbox";
+import {ComponentPreviews, useInitial} from "./dev";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
             <BaseUrlProvider>
-                <App />
+                <DevSupport ComponentPreviews={ComponentPreviews}
+                            useInitialHook={useInitial}
+                >
+                    <App/>
+                </DevSupport>
             </BaseUrlProvider>
         </BrowserRouter>
     </React.StrictMode>,
