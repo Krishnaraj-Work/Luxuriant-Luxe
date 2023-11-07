718bcbb0bc3c5029d0572a6d153f1c17093476e3
Krishnaraj Thadesar <krishnaraj.kpt@outlook.com>
2023-11-07 14:18:42 +0530

    added footer

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {ThemeContext} from "../context/ThemeContext";
-import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
 import '../style.css'
 import '../input.css'
 import '../css/Home.css'
+import {IconBrandFacebook, IconBrandInstagram, IconBrandTelegram, IconBrandWhatsapp} from "@tabler/icons-react";
 
 const Home = () => {
-
+	
 	const navigate = useNavigate();
 	const {theme} = React.useContext(ThemeContext);
 	useEffect(() => {
@@ -20,8 +20,7 @@ const Home = () => {
 		}
 	});
 	return (
-		<div>
-			<h1 className="bg-background text-text">Home</h1>
+		<div className="">
 			{/*<div className="untitled">*/}
 			{/*	<div className="untitled__slides">*/}
 			{/*		<div className="untitled__slide">*/}
@@ -60,6 +59,56 @@ const Home = () => {
 			{/*	</div>*/}
 			{/*	<div className="untitled__shutters"></div>*/}
 			{/*</div>*/}
+			
+			<section className="flex flex-col p-4 justify-center items-center" id="intro">
+				intro
+				<button className="btn btn-sm btn-primary">
+					View more
+				</button>
+			</section>
+			
+			<section className="flex flex-col p-4 justify-center items-center" id="our_products">
+				our products
+				<button className="btn btn-sm btn-primary">
+					Visit Store
+				</button>
+			</section>
+			
+			<section className="flex flex-col p-4 justify-center items-center" id="special_about_us">
+				special about us
+				<button className="btn btn-sm btn-primary">
+					Learn More
+				</button>
+			</section>
+			
+			<section className="flex flex-col p-4 justify-center items-center" id="contact">
+				contact us
+			</section>
+			
+			<footer className="footer footer-center p-10 bg-primary text-primary-content bottom">
+				<aside>
+					<div id="luxelogo" className="w-12 h-12 bg-center snap-center self-center"
+					></div>
+					<p><span className="text-lg">Luxuriant Luxe</span><br/>Your Skin, Our Priority</p>
+					<p>Copyright © 2023 - All right reserved</p>
+				</aside>
+				<nav>
+					<div className="grid grid-flow-col gap-4">
+						<a>
+							<IconBrandWhatsapp className="w-8 h-8"/>
+						</a>
+						<a>
+							<IconBrandInstagram className="w-8 h-8"/>
+						</a>
+						<a>
+							<IconBrandFacebook className="w-8 h-8"/>
+						</a>
+						<a>
+							<IconBrandTelegram className="w-8 h-8"/>
+						</a>
+					</div>
+				</nav>
+			</footer>
 		</div>
 	)
 }
